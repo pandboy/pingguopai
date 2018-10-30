a4a38d1024b1e2c1e3c3f279648d86fb4e9b21f4
Xie Fengling <flxie@best-inc.com>
2018-10-30 14:18:50 +0800

    删除评论2

--- src/main/java/com/xfl/pingguopai/configurer/MybatisConfigurer.java
+++ src/main/java/com/xfl/pingguopai/configurer/MybatisConfigurer.java
@@ -56,7 +56,7 @@ public class MybatisConfigurer {
         //配置通用Mapper，详情请查阅官方文档
         Properties properties = new Properties();
         properties.setProperty("mappers", MAPPER_INTERFACE_REFERENCE);
-        properties.setProperty("notEmpty", "false");//insert、update是否判断字符串类型!='' 即 test="str != null"表达式内是否追加 and str != ''
+        properties.setProperty("notEmpty", "false");
         properties.setProperty("IDENTITY", "MYSQL");
         mapperScannerConfigurer.setProperties(properties);
 
